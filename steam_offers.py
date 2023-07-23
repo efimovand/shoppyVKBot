163f01c2ec874d0884792b8b566d68de6cf425a9
efimovand <100632223+efimovand@users.noreply.github.com>
2023-07-23 18:43:32 +0400

    Fixed and fully debugged the steam trade offers sending function

--- steam_offers.py
+++ steam_offers.py
@@ -5,7 +5,7 @@ import configure
 
 # Авторизация в Steam
 steam_client = SteamClient(configure.steam_apiKey)
-steam_client.login(configure.steam_username, configure.steam_password, configure.steam_guard)
+steam_client.login(configure.steam_username, configure.steam_password, "steam_guard_Egor.json")
 
 
 # Поиск ID предмета в инвентаре по его названию
@@ -22,14 +22,18 @@ def find_item_id(item_hash_name, items):
 
 # Отправка трейда
 def sendTradeOffer(give_item, trade_link):
+
     game = GameOptions.CS
     my_inventory = steam_client.get_my_inventory(game)  # Получение своего инвентаря CS:GO
     my_item = find_item_id(give_item, my_inventory)  # Поиск ID нужного предмета в инвентаре
     my_item = [Asset(my_item['id'], game)]
 
-    steam_client.make_offer_with_url(my_item, [], trade_link)
+    try:
+        steam_client.make_offer_with_url(my_asset, [], trade_link)
+    except Exception as e:
+        print(e)
 
 
-item_to_send = ''  # Название предмета для отправки
-buyer_trade_link = ''  # Трейд ссылка покупателя
+item_to_send = 'Five-SeveN | Forest Night (Field-Tested)'  # Название предмета для отправки
+buyer_trade_link = 'https://steamcommunity.com/tradeoffer/new/?partner=923326400&token=sks89PCo'  # Трейд ссылка покупателя
 sendTradeOffer(item_to_send, buyer_trade_link)  # Отправка обмена
